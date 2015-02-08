a90db312cd57ceb528935017c7e0fb6da3a09d4f
ahlunju <ahlunju@hotmail.com>
2015-02-08 15:56:46 -0500

    implemented pagination

--- app/scripts/controllers/clients.js
+++ app/scripts/controllers/clients.js
@@ -12,21 +12,33 @@ angular.module('clientsApp')
     $rootScope.clients = localStorageService.get('clients');
     // console.log($rootScope.clients);
     if (!$rootScope.clients) {
-        var promise = Clientsservice.getClients();
-        promise.then(function(payload) {
-            $rootScope.clients = payload;
-            console.log('got clients from json...');
+      var promise = Clientsservice.getClients();
+      promise.then(function(payload) {
+        $rootScope.clients = payload;
+        console.log('got clients from json...');
 
-            localStorageService.set('clients', payload);
-            
-        }, function(error) {
-            $log.error('something went wrong...');
-        });
+        localStorageService.set('clients', payload);
+          
+      }, function(error) {
+        $log.error('something went wrong...');
+      });
     }
     
     $scope.editClient = function(id) {
-        $location.path('edit/'+id);
+      $location.path('edit/'+id);
     }
+    
+    $scope.$watch('[currentPage, itemPerPage]', function() {
+      $scope.begin = ($scope.currentPage - 1) * $scope.itemPerPage;
+      $scope.end = $scope.begin + $scope.itemPerPage;
+      $scope.filteredClients = $rootScope.clients.slice($scope.begin, $scope.end);
+    });
 
+    
     $scope.orderByField = 'id';
+    $scope.totalItems = $rootScope.clients.length;
+    $scope.currentPage = 1;
+    $scope.numPage = Math.ceil($rootScope.clients.length / $scope.itemPerPage);
+    $scope.itemPerPage = 3;
+
   });
